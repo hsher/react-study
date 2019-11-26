c9c8d3ae4a78207841bbffee243dc4bbdefeacdc
Yurii Mochalov <ym@anahoret.com>
2019-11-26 11:35:35 +0200

    Use App from book instead of default

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+const App = () => {
+  return <p>Всем привет, я компонент App</p>
+}
 
 const News = () => {
   return <p>К сожалению, новостей нет</p>
