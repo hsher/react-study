f2a171d3db0c4b5cc0ab89ad0f16383b36730715
Yurii Mochalov <ym@anahoret.com>
2019-11-26 15:48:10 +0200

    Add handler to readmore link

--- src/index.js
+++ src/index.js
@@ -44,6 +44,11 @@ class Article extends React.Component {
     visible: false
   }
 
+  handleReadMoreClck = (e) => {
+    e.preventDefault()
+    this.setState({ visible: true })
+  }
+
   render() {
     const { author, text, bigText } = this.props.data
     const { visible } = this.state
@@ -53,7 +58,7 @@ class Article extends React.Component {
         <p className="news__author">{author}:</p>
         <p className="news__text">{text}</p>
         {
-          !visible && <a href='#' className='news__readmore'>Подробнее</a>
+          !visible && <a onClick={this.handleReadMoreClck} href='#' className='news__readmore'>Подробнее</a>
         }
         {
           visible && <p className="news__big-text">{bigText}</p>
