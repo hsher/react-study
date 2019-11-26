2fe33846db2071034441d7a3e74f7091b3f17e06
Yurii Mochalov <ym@anahoret.com>
2019-11-26 15:38:17 +0200

    Update data with bigText

--- src/index.js
+++ src/index.js
@@ -7,22 +7,26 @@ const myNews = [
   {
     id: 1,
     author: 'Саша Печкин',
-    text: 'В четверг, четвертого числа...'
+    text: 'В четверг, четвертого числа...',
+    bigText: 'в четыре с четвертью часа четыре чёрненьких чумазеньких чертёнка чертили чёрными чернилами чертёж.'
   },
   {
     id: 2,
     author: 'Просто Вася',
-    text: 'Считаю, что $ должен стоить 35 рублей!'
+    text: 'Считаю, что $ должен стоить 35 рублей!',
+    bigText: 'А евро 42!'
   },
   {
     id: 3,
     author: 'Max Frontend',
-    text: 'Прошло 2 года с прошлых учебников, а $ так и не стоит 35'
+    text: 'Прошло 2 года с прошлых учебников, а $ так и не стоит 35',
+    bigText: 'А евро опять выше 70.'
   },
   {
     id: 4,
     author: 'Гость',
-    text: 'Бесплатно. Без смс, про рект, заходи - https://maxpfrontend.ru'
+    text: 'Бесплатно. Без смс, про рект, заходи - https://maxpfrontend.ru',
+    bigText: 'Еще есть группа VK, telegram и канал на youtube! Вся инфа на сайте, не реклама!'
   }
 ]
 
@@ -37,11 +41,12 @@ const App = () => {
 
 class Article extends React.Component {
   render() {
-    const { author, text } = this.props.data
+    const { author, text, bigText } = this.props.data
     return (
       <div className="article">
         <p className="news__author">{author}:</p>
         <p className="news__text">{text}</p>
+        <p className="news__big-text">{bigText}</p>
       </div>
     )
   }
@@ -50,7 +55,8 @@ class Article extends React.Component {
 Article.propTypes = {
   data: PropTypes.shape({
     author: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired
+    text: PropTypes.string.isRequired,
+    bigText: PropTypes.string.isRequired
   })
 }
 
