45ad42e50be33a9da3ea045279cb63b7bcf0701c
Yurii Mochalov <ym@anahoret.com>
2019-11-26 11:26:43 +0200

    Create component by class

--- src/index.js
+++ src/index.js
@@ -3,4 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class BigApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Я компонент, BigApp</h1>
+        <p className='red'>Компоненты можно вкладывать друг в друга.</p>
+        <App />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<BigApp />, document.getElementById('root'));
