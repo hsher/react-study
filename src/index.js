4e5d48003b2fdb71ee45598c04511750342501de
Yurii Mochalov <ym@anahoret.com>
2019-11-26 14:12:43 +0200

    Make statefull News

--- src/index.js
+++ src/index.js
@@ -2,10 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const myNews = [
+  {
+    author: 'Саша Печкин',
+    text: 'В четверг, четвертого числа...'
+  },
+  {
+    author: 'Просто Вася',
+    text: 'Считаю, что $ должен стоить 35 рублей!'
+  },
+  {
+    author: 'Max Frontend',
+    text: 'Прошло 2 года с прошлых учебников, а $ так и не стоит 35'
+  },
+  {
+    author: 'Гость',
+    text: 'Бесплатно. Без смс, про рект, заходи - https://maxpfrontend.ru'
+  }
+]
+
 const App = () => {
   return (
     <React.Fragment>
-      <News />
+      <News data={myNews} />
       <Comments />
     </React.Fragment>
   )
@@ -15,8 +34,25 @@ const Comments = () => {
   return <div>Нет новостей - комментировать нечего.</div>
 }
 
-const News = () => {
-  return <p>К сожалению, новостей нет</p>
+class News extends React.Component {
+  render() {
+    const newsTemplate = this.props.data.map(function(item, index) {
+      return (
+        <div key={index}>
+          <p className="news__author">{item.author}</p>
+          <p className="news__text">{item.text}</p>
+        </div>
+      )
+    })
+
+    console.log(newsTemplate)
+
+    return (
+      <div className="news">
+        {newsTemplate}
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
