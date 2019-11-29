ae20ed209b3b9b687cc64875c6e84ab2e480b4aa
Yurii Mochalov <ym@anahoret.com>
2019-11-29 10:05:28 +0200

    Extract Article to own component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types'
+import { Article } from './components/Article'
 import './App.css';
 
 const myNews = [
@@ -30,45 +31,6 @@ const myNews = [
       "Еще есть группа VK, telegram и канал на youtube! Вся инфа на сайте, не реклама!"
   }
 ];
-
-class Article extends React.Component {
-  state = {
-    visible: false
-  };
-  handleReadMoreClck = e => {
-    e.preventDefault();
-    this.setState({ visible: true });
-  };
-  render() {
-    const { author, text, bigText } = this.props.data;
-    const { visible } = this.state;
-    return (
-      <div className="article">
-        <p className="news__author">{author}:</p>
-        <p className="news__text">{text}</p>
-        {!visible && (
-          <a
-            onClick={this.handleReadMoreClck}
-            href="#readmore"
-            className="news__readmore"
-          >
-            Подробнее
-          </a>
-        )}
-        {visible && <p className="news__big-text">{bigText}</p>}
-      </div>
-    );
-  }
-}
-
-Article.propTypes = {
-  data: PropTypes.shape({
-    id: PropTypes.number.isRequired, // добавили id, это число, обязательно
-    author: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired,
-    bigText: PropTypes.string.isRequired
-  })
-};
 
 class News extends React.Component {
   renderNews = () => {
