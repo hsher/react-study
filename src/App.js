a9a2a9cc34be7b3a177d3fd8d88a027fa232e281
Yurii Mochalov <ym@anahoret.com>
2019-11-29 13:54:11 +0200

    Add async

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import React from 'react';
-import { News } from './components/News'
-import { Add } from './components/Add'
-import newsData from './data/newsData'
+import { News } from './components/News';
+import { Add } from './components/Add';
 import './App.css';
 
 class App extends React.Component {
   state = {
-    news: newsData
+    news: null,
+    isLoading: false,
   };
+  componentDidMount () {
+    this.setState({ isLoading: true });
+    fetch('http://localhost:3000/data/newsData.json')
+      .then(response => response.json())
+      .then(data => {
+        setTimeout(() => { // добавили задержку
+          this.setState({ isLoading: false, news: data });
+        }, 3000); // в три секунды
+      });
+  }
   handleAddNews = data => {
     const nextNews = [data, ...this.state.news];
     this.setState({ news: nextNews });
   };
-  render() {
+  render () {
+    const { news, isLoading } = this.state;
+
     return (
       <React.Fragment>
         <Add onAddNews={this.handleAddNews} />
         <h3>Новости</h3>
-        <News data={this.state.news} />
+        {isLoading && <p>Загружаю...</p>}
+        {Array.isArray(news) && <News data={news} />}
       </React.Fragment>
     );
   }
