33b31a3fb6d016a5ac37d8e1a2516ade3e73d16d
Yurii Mochalov <ym@anahoret.com>
2019-11-29 10:10:19 +0200

    Extract News to component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types'
-import { Article } from './components/Article'
+import { News } from './components/News'
 import './App.css';
 
 const myNews = [
@@ -31,41 +31,6 @@ const myNews = [
       "Еще есть группа VK, telegram и канал на youtube! Вся инфа на сайте, не реклама!"
   }
 ];
-
-class News extends React.Component {
-  renderNews = () => {
-    const { data } = this.props;
-    let newsTemplate = null;
-
-    if (data.length) {
-      newsTemplate = data.map(function(item) {
-        return <Article key={item.id} data={item} />;
-      });
-    } else {
-      newsTemplate = <p>К сожалению новостей нет</p>;
-    }
-
-    return newsTemplate;
-  };
-  render() {
-    const { data } = this.props;
-
-    return (
-      <div className="news">
-        {this.renderNews()}
-        {data.length ? (
-          <strong className={"news__count"}>
-            Всего новостей: {data.length}
-          </strong>
-        ) : null}
-      </div>
-    );
-  }
-}
-
-News.propTypes = {
-  data: PropTypes.array.isRequired
-};
 
 class Add extends React.Component {
   state = {
