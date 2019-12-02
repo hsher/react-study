35f5fd19b54037b0a3d5fb2c644af3476fe3342e
Yurii Mochalov <ym@anahoret.com>
2019-12-02 10:57:17 +0200

    Check spam in App

--- src/App.js
+++ src/App.js
@@ -8,6 +8,26 @@ class App extends React.Component {
     news: null,
     isLoading: false,
   };
+  static getDerivedStateFromProps(props, state) {
+    let nextFilteredNews
+
+    if (Array.isArray(state.news)) {
+      nextFilteredNews = [...state.news]
+
+      nextFilteredNews.forEach((item, index) => {
+        if (item.bigText.toLowerCase().indexOf('pubg') !== -1) {
+          item.bigText = 'СПАМ'
+        }
+      })
+
+      return {
+        filteredNews: nextFilteredNews,
+      }
+    }
+
+    return null
+  }
+
   componentDidMount () {
     this.setState({ isLoading: true });
     fetch('http://localhost:3000/data/newsData.json')
